92b290921a2c1dd88a674c4c04a62c950dce3f80
Molnár Ádám <madam@pocketgames.eu>
2018-08-29 17:13:34 +0200

    nice jump ;)

--- Elemort/Assets/Scripts/PlayerMovement.cs
+++ Elemort/Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,32 @@ public class PlayerMovement : MonoBehaviour {
 
     public float speed;
 
-    void Start() {
+    public float jumpHeight;
 
+    private float distanceFromGround;
+
+    void Start() {
+        distanceFromGround = GetComponent<BoxCollider2D>().bounds.extents.y;
     }
 
     void Update() {
         var x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+        var jumpedButtonPressed = Input.GetButton("Jump");
+        if (jumpedButtonPressed && IsPlayerOnGround())
+            Jump();
+
         transform.Translate(x, 0, 0);
+    }
+
+    private void Jump() {
+        Debug.Log("jumped");
+        var asd = GetComponent<Rigidbody2D>();
+        asd.velocity = new Vector2(0, jumpHeight);
+    }
+
+    private bool IsPlayerOnGround() {
+        var dist = Physics.Raycast(transform.position, -Vector3.up, distanceFromGround + 0.1f);
+        return dist;
     }
 
 }
