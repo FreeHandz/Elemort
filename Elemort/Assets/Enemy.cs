f4365da6a3e6fc2366f8293216fa11e04a10b5a7
Héjja Norbert <nhejja@pocketgames.eu>
2018-08-30 17:05:23 +0200

    nem

---
+++ Elemort/Assets/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public int health;
+
+	public void takeDamage(int damage, bool fromCard = false)
+	{
+		health -= damage;
+		if (health <= 0)
+		{
+			Die ();
+		}
+	}
+
+	public void Die()
+	{
+		// TODO enemy died
+		gameObject.SetActive(false);
+	}
+}
